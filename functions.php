2a398582d40374f1af16e26e25c5bc4dc1301dac
pbodsk <me@peterbodskov.dk>
2010-10-25 21:28:51 +0200

    working on headers, entries and sidebar

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ if(function_exists('register_sidebar')){
 	register_sidebar(array(
 	'before_widget' => '',
 	'after_widget' => '</div>',
-	'before_title' => '<h2 class="widgetTitle">',
+	'before_title' => '<h2 class="widgetTitle entryHeader">',
 	'after_title' => '</h2><div class="small">',
 	'name' => 'Sidebar'
 	));
